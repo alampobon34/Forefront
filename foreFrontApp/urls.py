483736ca4506274cecfa529b20e8fc634ef937a1
alampobon34 <alampobon34@gmail.com>
2022-01-31 20:15:29 +0600

    edited

--- foreFrontApp/urls.py
+++ foreFrontApp/urls.py
@@ -3,7 +3,7 @@ from foreFrontApp.views import *
 
 
 urlpatterns = [
-    path("", index, name='home'),
+    path("/", index, name='home'),
     path("about-us/", about_us, name='about-us'),
     path("news-details/<int:id>/", news_details, name='news-details'),
     path("subscribe", subscribe, name='subscribe'),
